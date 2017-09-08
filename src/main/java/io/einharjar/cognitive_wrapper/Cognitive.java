cf552a2209828451be03a88702dda5f390608e29
Omar Olivares <oaro@kth.se>
2017-09-09 00:37:45 +0200

    Allow caller to supply own instance of OkHttpClient. More work on async api

--- src/main/java/io/einharjar/cognitive_wrapper/Cognitive.java
+++ src/main/java/io/einharjar/cognitive_wrapper/Cognitive.java
@@ -2,7 +2,10 @@ package io.einharjar.cognitive_wrapper;
 
 import io.einharjar.cognitive_wrapper.computer_vision.VisionClient;
 import okhttp3.OkHttpClient;
+
 import java.net.MalformedURLException;
+
+import static io.einharjar.cognitive_wrapper.utils.ObjectHelper.checkNull;
 
 public class Cognitive {
     private OkHttpClient httpClient;
@@ -10,12 +13,21 @@ public class Cognitive {
     private ApiSettings apiSettings;
 
     public Cognitive(ApiSettings apiSettings) throws MalformedURLException {
+        checkNull(apiSettings, "API Settings cannot be null!");
         this.httpClient = new OkHttpClient();
         this.apiSettings = apiSettings;
         this.visionClient = new VisionClient(this.apiSettings, this.httpClient);
     }
 
-    public VisionClient getVisionClient(){
+    public Cognitive(ApiSettings apiSettings, OkHttpClient okHttpClient) throws MalformedURLException {
+        checkNull(apiSettings, "API Settings cannot be null!");
+        checkNull(okHttpClient, "okHttpClient cannot be null!");
+        this.httpClient = okHttpClient;
+        this.apiSettings = apiSettings;
+        this.visionClient = new VisionClient(this.apiSettings, this.httpClient);
+    }
+
+    public VisionClient getVisionClient() {
         return this.visionClient;
     }
 
--- src/main/java/io/einharjar/cognitive_wrapper/computer_vision/VisionClientAsync.java
+++ src/main/java/io/einharjar/cognitive_wrapper/computer_vision/VisionClientAsync.java
@@ -4,12 +4,18 @@ import io.einharjar.cognitive_wrapper.ApiSettings;
 import io.einharjar.cognitive_wrapper.async.ResponseCallback;
 import io.einharjar.cognitive_wrapper.computer_vision.error.ApiException;
 import io.einharjar.cognitive_wrapper.computer_vision.response.DescribeImageResponse;
+import io.einharjar.cognitive_wrapper.utils.ImageHelper;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
+
 import static io.einharjar.cognitive_wrapper.computer_vision.VisionClient.handleResponse;
 import static io.einharjar.cognitive_wrapper.computer_vision.VisionRequests.describeRequest;
 import static io.einharjar.cognitive_wrapper.utils.ObjectHelper.checkNull;
@@ -24,8 +30,37 @@ public class VisionClientAsync {
         this.client = client;
     }
 
+    public void describeImage(int maxCandidates, BufferedImage image, final ResponseCallback<DescribeImageResponse> cb) throws IOException {
+        describeImageAsync(maxCandidates, ImageHelper.readImage(image), cb);
+    }
+
+
+    public void describeImage(int maxCandidates, File image, final ResponseCallback<DescribeImageResponse> cb) throws IOException {
+        describeImageAsync(maxCandidates, ImageHelper.readImage(image), cb);
+    }
+
+    public void describeImageAsync(int maxCandidates, URL url, final ResponseCallback<DescribeImageResponse> cb) throws IOException {
+        describeImageAsync(maxCandidates, url.toString(), cb);
+    }
+
     public void describeImageAsync(int maxCandidates, String url, final ResponseCallback<DescribeImageResponse> cb) throws IOException {
         client.newCall(describeRequest(maxCandidates, url, apiSettings)).enqueue(new Callback() {
+            public void onFailure(Call call, IOException e) {
+                cb.onFailure(e);
+            }
+
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    cb.onSuccess(handleResponse(response, DescribeImageResponse.class));
+                } catch (ApiException e) {
+                    cb.onFailure(e.getError());
+                }
+            }
+        });
+    }
+
+    public void describeImageAsync(int maxCandidates, byte[] image, final ResponseCallback<DescribeImageResponse> cb) throws IOException {
+        client.newCall(describeRequest(maxCandidates, image, apiSettings)).enqueue(new Callback() {
             public void onFailure(Call call, IOException e) {
                 cb.onFailure(e);
             }
